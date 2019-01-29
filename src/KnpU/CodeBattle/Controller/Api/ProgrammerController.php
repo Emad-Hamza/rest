75de677403e580aaf8e4bc801178b9cddaaad43c
DESKTOP-KRRLU6D\Mahmoud S. Siddiq <emad.ahmed@netservex.com>
2019-01-29 18:39:22 +0200

    5 Project Routing

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -3,6 +3,7 @@
 namespace KnpU\CodeBattle\Controller\Api;
 
 use KnpU\CodeBattle\Controller\BaseController;
+use KnpU\CodeBattle\Model\Programmer;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,32 @@ class ProgrammerController extends BaseController
 {
     protected function addRoutes(ControllerCollection $controllers)
     {
-        // $controllers->post('/api/programmers', array($this, 'newAction'));
+         $controllers->post('/api/programmers', array($this, 'newAction'));
+    }
+
+    public function newAction(Request $request)
+    {
+
+
+        $data = json_decode($request->getContent(), true);
+
+        $programmer = new Programmer();
+
+        $programmer->nickname = $data['nickname'];
+        $programmer->avatarNumber = $data['avatarNumber'];
+        $programmer->tagLine = $data['tagLine'];
+        $programmer->userId = $this->findUserByUsername('weaverryan')->id;
+
+
+        try {
+            $this->save($programmer);
+        } catch (\Exception $e) {
+        }
+
+        $response = new Response("It works now!", 201);
+        $response->headers->set('Location', '/programmers/not/fake...');
+
+        return $response;
     }
 
 }
